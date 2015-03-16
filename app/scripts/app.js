f25a70e842f4398b6194f993425297f8bc95cf63
Knut Lorvik <lorviken@gmail.com>
2015-03-16 16:15:32 +0100

    Add angular application

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name gainmasterApp
+ * @description
+ * # gainmasterApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('gainmasterApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gainmasterApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gainmasterApp
+ */
+angular.module('gainmasterApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
